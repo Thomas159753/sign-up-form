882002dbe202a68cd446841aef26d79db0d66b1d
Thomas159753 <99398525+Thomas159753@users.noreply.github.com>
2023-02-05 18:43:29 +1100

    more work is done
    submit button is working

---
+++ javascript.js
@@ -0,0 +1,38 @@
+// const first_name = document.getElementById("first_name");
+// const last_name = document.getElementById("last_name");
+// const user_email = document.getElementById("user_email");
+// const phone_number = document.getElementById("phone_number");
+// const submit = document.getElementById("submit")
+// const mailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+// function validateform(){
+//     if (first_name.value === ""){
+//         first_name.setCustomValidity("First name required");
+//     }
+    
+//     if (last_name === "") {
+//         last_name.setCustomValidity("Last name required")
+//     }
+
+//     if(user_email.matches(mailValidation)){
+
+//     }
+// }
+
+// submit.addEventListener('click', function() {
+//     validateform();
+// })
+// // // var password = document.getElementById("password")
+// // //   , confirm_password = document.getElementById("confirm_password");
+
+// // // function validatePassword(){
+// // //   if(password.value != confirm_password.value) {
+// // //     confirm_password.setCustomValidity("Passwords Don't Match");
+// // //   } else {
+// // //     confirm_password.setCustomValidity('');
+// // //   }
+// // // }
+
+// // // password.onchange = validatePassword;
+// // // confirm_password.onkeyup = validatePassword;
