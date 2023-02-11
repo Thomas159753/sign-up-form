c882e54b85526034a2314198b5d5d9d68bfdaadd
Thomas159753 <99398525+Thomas159753@users.noreply.github.com>
2023-02-11 22:47:37 +1100

    finished

--- javascript.js
+++ javascript.js
@@ -1,38 +1,23 @@
-// const first_name = document.getElementById("first_name");
-// const last_name = document.getElementById("last_name");
-// const user_email = document.getElementById("user_email");
-// const phone_number = document.getElementById("phone_number");
-// const submit = document.getElementById("submit")
-// const mailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const password = document.getElementById("user_password");
+const confirm_password = document.getElementById("confirm_password");
+const submit = document.getElementById("submit");
+const passwordText = document.querySelector(".pasMatch");
+const validiti = document.querySelector(".validiti");
 
 
-// function validateform(){
-//     if (first_name.value === ""){
-//         first_name.setCustomValidity("First name required");
-//     }
-    
-//     if (last_name === "") {
-//         last_name.setCustomValidity("Last name required")
-//     }
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    password.style.border = "2px solid red";
+    confirm_password.style.border = "2px solid red";
+    passwordText.innerText = "* Passwords Don't Match";
 
-//     if(user_email.matches(mailValidation)){
+  } else {
+    confirm_password.setCustomValidity('');
+    passwordText.innerText = "";
+    password.style.border = "1px solid black";
+    confirm_password.style.border = "1px solid black";
+  }
+}
 
-//     }
-// }
-
-// submit.addEventListener('click', function() {
-//     validateform();
-// })
-// // // var password = document.getElementById("password")
-// // //   , confirm_password = document.getElementById("confirm_password");
-
-// // // function validatePassword(){
-// // //   if(password.value != confirm_password.value) {
-// // //     confirm_password.setCustomValidity("Passwords Don't Match");
-// // //   } else {
-// // //     confirm_password.setCustomValidity('');
-// // //   }
-// // // }
-
-// // // password.onchange = validatePassword;
-// // // confirm_password.onkeyup = validatePassword;
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
